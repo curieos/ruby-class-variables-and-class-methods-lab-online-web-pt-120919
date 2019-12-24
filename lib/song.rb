4f498700bc50bb3e3a6f91d9b918087cdf882d16
Caroline Longnecker <cvrieous@gmail.com>
2019-12-24 04:45:14 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,11 +2,15 @@ class Song
   attr_accessor :name, :artist, :genre
   
   @@count = 0
+  @@artists = []
+  @@genres = []
   
   def initialize(name, artist, genre)
     @name = name
     @artist = artist
     @genre = genre
+    
     @@count += 1
+    @@artists << artist
   end
 end
